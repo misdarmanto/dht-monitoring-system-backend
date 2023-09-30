12052a1917fff5e12064f12c699ee1f3948e3f5a
misdarmanto <misdarmanto1922@gmail.com>
2023-09-30 20:54:18 +0700

    filter data by date time

--- apps/controllers/dht_sensor/find.ts
+++ apps/controllers/dht_sensor/find.ts
@@ -6,8 +6,25 @@ import { Pagination } from '../../utilities/pagination'
 import { requestChecker } from '../../utilities/requestCheker'
 import { CONSOLE } from '../../utilities/log'
 import { type DhtSensorAttributes, DhtSensorModel } from '../../models/dht_sensor'
+import moment from 'moment'
 
 export const findAllDhtSensor = async (req: any, res: Response): Promise<any> => {
+  const TODAY_START = moment().startOf('day').toDate()
+  const TODAY_END = moment().endOf('day').toDate()
+
+  const WEEK_START = moment().startOf('week').toDate()
+  const WEEK_END = moment().endOf('week').toDate()
+
+  const MONTH_START = moment().startOf('month').toDate()
+  const MONTH_END = moment().endOf('month').toDate()
+
+  const YEAR_START = moment().startOf('year').toDate()
+  const YEAR_END = moment().endOf('year').toDate()
+
+  const YESTERDAY = moment().clone().subtract(1, 'day')
+  const YESTERDAY_START = YESTERDAY.startOf('day').toDate()
+  const YESTERDAY_END = YESTERDAY.endOf('day').toDate()
+
   try {
     const page = new Pagination(
       parseInt(req.query.page) ?? 0,
@@ -21,14 +38,26 @@ export const findAllDhtSensor = async (req: any, res: Response): Promise<any> =>
             { dhtSensorHumidity: { [Op.like]: `%${req.query.search}%` } },
             { dhtSensorTemperature: { [Op.like]: `%${req.query.search}%` } }
           ]
+        }),
+        ...(req.query.range === 'today' && {
+          createdAt: { [Op.between]: [TODAY_START, TODAY_END] }
+        }),
+        ...(req.query.range === 'yesterday' && {
+          createdAt: {
+            [Op.between]: [YESTERDAY_START, YESTERDAY_END]
+          }
+        }),
+        ...(req.query.range === 'week' && {
+          createdAt: { [Op.between]: [WEEK_START, WEEK_END] }
+        }),
+        ...(req.query.range === 'month' && {
+          createdAt: { [Op.between]: [MONTH_START, MONTH_END] }
+        }),
+        ...(req.query.range === 'year' && {
+          createdAt: { [Op.between]: [YEAR_START, YEAR_END] }
         })
-        // createdAt: {
-        //   [Op.between]: [
-        //     new Date('2023-09-30T07:35:43.000Z'),
-        //     new Date('2023-09-30T07:38:20.000Z')
-        //   ]
-        // }
       },
+
       order: [['id', 'desc']],
       ...(req.query.pagination === 'true' && {
         limit: page.limit,
